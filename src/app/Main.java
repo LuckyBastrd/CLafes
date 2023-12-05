9a22af2575062825e57b0dbcfc027f8a67ac1a85
ㅤKhairunizham <rafi.khairunizham@gmail.com>
2023-12-05 20:27:50 +0700

    first commit

---
+++ src/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+public class Main {
+
+	public Main() {
+		System.out.println("Tes Push");
+	}
+
+	public static void main(String[] args) {
+		new Main();
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,3 @@
+module CLafes {
+	opens app;
+}
